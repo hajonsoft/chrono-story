bb9d3956319607a1cd2b26138c69ce8d52c59d90
AymanA <aliayman@alumni.harvard.edu>
2024-03-25 04:55:31 -0700

    fix: lint hos-000

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 const auth = getAuth(app); // Initialize Firebase authentication service
 const googleAuthProvider = new GoogleAuthProvider(); // Create Google authentication provider
