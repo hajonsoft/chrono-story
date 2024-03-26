51e80383d2c607e38c8efb27b56aafc3136189c4
AymanA <aliayman@alumni.harvard.edu>
2024-03-26 09:39:55 -0700

    listen to changes

--- src/components/TimeLine/Display.jsx
+++ src/components/TimeLine/Display.jsx
@@ -1,72 +1,60 @@
-import React, { useState, useEffect } from "react";
-import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { Box, Card, Stack, Typography } from "@mui/material";
 import { getAuth } from "firebase/auth";
-import { Box, Stack } from "@mui/material";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
 const TimelineDisplay = ({ mode }) => {
   const [timeline, setTimeline] = useState([]);
 
   useEffect(() => {
-    const fetchTimeline = async () => {
-      try {
-        const auth = getAuth();
-        const firestore = getFirestore();
-        // Get the current user's UID
-        const userId = auth.currentUser.uid;
+    const auth = getAuth();
+    const db = getFirestore();
+    const userId = auth.currentUser.uid;
 
-        // Reference to the document containing the timeline array
-        const userDocRef = doc(firestore, `users/${userId}`);
+    const unsubscribe = onSnapshot(doc(db, "users", userId), (doc) => {
+      const timeLineData = doc.data().timeLine;
+      setTimeline(timeLineData);
+    });
 
-        // Fetch the document data
-        const docSnapshot = await getDoc(userDocRef);
-        if (docSnapshot.exists()) {
-          // Extract the timeline array from the document data
-          const userTimeline = (await docSnapshot.data().timeLine) || [];
-          setTimeline(userTimeline);
-        } else {
-          console.error("Document does not exist");
-        }
-      } catch (error) {
-        console.error("Error fetching timeline: ", error);
-      }
-    };
-
-    fetchTimeline(); // Fetch timeline when component mounts
+    return unsubscribe;
   }, [mode]); // Empty dependency array ensures useEffect runs only once
 
   return (
-    <Box
-      sx={{
-        border: "1px solid brown",
-        borderRadius: "16px",
-        margin: "16px",
-        padding: "16px",
-      }}
-    >
+    <Box>
       <ul>
         {timeline.map((entry, index) => (
-          <Stack key={index} spacing={2}>
-            <h3>{entry.year}</h3>
-            <h4>{entry.title}</h4>
-            <p>{entry.description}</p>
-            <img
-              src={entry.image}
-              alt={entry.title}
-              style={{ width: "100px" }}
-            />
-            <ul>
-              {entry.photos?.map((photo, index) => (
-                <li key={index}>
-                  <img
-                    src={photo.image}
-                    alt={photo.title}
-                    style={{ width: "50px" }}
-                  />
-                  <p>{photo.title}</p>
-                </li>
-              ))}
-            </ul>
-          </Stack>
+          <Card key={index} sx={{ padding: "16px", margin: "16px" }}>
+            <Stack key={index} spacing={2} direction={"row"}>
+              <Typography variant="h5" sx={{ width: "8%" }}>
+                {entry.year}
+              </Typography>
+              <img
+                style={{ width: "100px", height: '100px' }}
+                src={entry.image || "https://source.unsplash.com/random"}
+                alt={entry.title}
+              />
+              <Stack direction={"column"}>
+                <Typography variant="subtitle" align="left" gutterBottom>
+                  {entry.title}
+                </Typography>
+                <Typography variant="body1" align="left">
+                  {entry.description}
+                </Typography>
+                <ul>
+                  {entry.photos?.map((photo, index) => (
+                    <li key={index}>
+                      <img
+                        src={photo.image}
+                        alt={photo.title}
+                        style={{ width: "50px" }}
+                      />
+                      <p>{photo.title}</p>
+                    </li>
+                  ))}
+                </ul>
+              </Stack>
+            </Stack>
+          </Card>
         ))}
       </ul>
     </Box>
